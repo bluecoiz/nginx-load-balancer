58ddb3196af4ff53bc6de8d7aa79c485de166d41
root <root@fedora.mshome.net>
2020-02-06 17:55:01 +0800

    clean up

--- nginx/build-container.sh
+++
@@ -1,2 +0,0 @@
-podman container run -p 5000:80 --name loadbalancer --network cni-podman1  -d nginx-lb:001
-podman ps
--- nodejs/build-container.sh
+++
@@ -1,3 +0,0 @@
-podman container run -p 5001:5000 --name nodeapp-1 -d nodeapp:001
-podman container run -p 5002:5000 --name nodeapp-2 -e "name=你好啊！！！" -d nodeapp:001
-podman ps
